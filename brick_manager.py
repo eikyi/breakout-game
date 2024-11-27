1d98b2476ebb56fc76f8bd626d10184ef2c093e1
eikyi <eikyi27@gmail.com>
2024-11-27 13:58:24 +1100

    Added class to lay bricks and detect collisions added ball class for move ball functions Updated the main to complete the game

--- brick_manager.py
+++ brick_manager.py
@@ -1,8 +1,7 @@
-import random
+
 from turtle import Turtle
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-POSITIONS = [()]
+COLORS = ["red", "orange", "yellow","green"]
 
 class BrickManager:
 
@@ -10,12 +9,26 @@ class BrickManager:
         self.all_bricks = []
 
     def create_bricks(self):
+        #brick starting position
+        x,y= -350,100
+        #lay the bricks
+        for i in range(len(COLORS)):
+            while x < 380:
+                new_brick = Turtle("square")
+                new_brick.color(COLORS[i])
+                new_brick.penup()
+                new_brick.shapesize(stretch_len=3, stretch_wid=2)
+                new_brick.goto(x, y)
+                self.all_bricks.append(new_brick)
+                x += 70
+            x= -350
+            y += 50
 
-        new_brick = Turtle("square")
-        new_brick.color(random.choice(COLORS))
-        new_brick.penup()
-        new_brick.shapesize(stretch_len=3, stretch_wid=2)
-        random_y = random.randint(-250, 250)
-        new_brick.goto(300, random_y)
-        self.all_bricks.append(new_brick)
+    def detect_collision(self,ball):
 
+        for brick in self.all_bricks:
+            #check ball coordinates and brick coordinates for collision
+            if ball.distance(brick) < 50 and brick.xcor()-50 <= ball.xcor() <= brick.xcor()+50 and brick.ycor() - 50 <= ball.ycor() <= brick.ycor()+50:
+                #delete the turtle from screen
+                brick.reset()
+                return True
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
 from turtle import Turtle, Screen
 from paddle import Paddle
+from brick_manager import BrickManager
+from ball import Ball
 
+FONT = ("Courier", 30, "normal")
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=900)
@@ -9,13 +13,42 @@ screen.tracer(0)
 
 #initialize the paddle
 paddle = Paddle()
-
+brick = BrickManager()
+brick.create_bricks()
+ball = Ball()
 screen.listen()
 screen.onkey(key="Left",fun=paddle.go_left)
 screen.onkey(key="Right",fun=paddle.go_right)
 
 game_is_on = True
 while game_is_on:
+    #smooth out the ball movement
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+    # Detect collision with side wall.
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.bounce_x()
+    # Detect collision with top wall.
+    if ball.ycor() > 430:
+        ball.bounce_y()
+    # Detect collision with brick and ball
+    if brick.detect_collision(ball):
+        #after deleting the brick after collision update the screen
+        screen.update()
+        #bounce the ball
+        ball.bounce_y()
+    #Detect collision with paddle
+    if ball.distance(paddle) < 50 and ball.ycor() < -380:
+        ball.bounce_y()
+    #Detect missed to catch the ball
+    if ball.ycor() < -500:
+        game_is_on = False
+        #Write Game over on screen
+        game_over = Turtle()
+        game_over.color("white")
+        game_over.penup()
+        game_over.goto(0, 0)
+        game_over.write(f"GAME OVER", align="center", font=FONT)
 
 screen.exitonclick()
